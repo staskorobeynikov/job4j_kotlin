57af21679b44763a36335e6368554998571cc10f
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-02-04 21:51:04 +0300

    1. KDoc. Документация. [#381430]

--- src/main/kotlin/ru/job4j/bank/BankService.kt
+++ src/main/kotlin/ru/job4j/bank/BankService.kt
@@ -1,28 +1,53 @@
 package ru.job4j.bank
 
+/**
+ * Класс описывает сервис банковских переводов.
+ */
 class BankService {
     private val users: MutableMap<User, MutableList<Account>> = mutableMapOf()
 
+    /**
+     * Метод добавляет [user] в карту и ассоциированный с ним пустой список аккаунтов.
+     */
     fun addUser(user: User) {
         users.putIfAbsent(user, mutableListOf())
     }
 
+    /**
+     * Метод добавляет [account] в список аккаунтов пользователя, у которого совпал [passport].
+     */
     fun addAccount(passport: String, account: Account) {
         findByPassport(passport)?.let {
             user -> users[user]?.add(account)
         }
     }
 
+    /**
+     * Метод поиска пользователя по [passport]
+     * @return пользователь, у которого паспортные данные равны переданному параметру [passport].
+     */
     fun findByPassport(passport: String): User? {
         return users.keys.find { it.passport == passport }
     }
 
+    /**
+     * Метод ищет аккаунт по [passport] и [requisite]
+     * @return аккаунт, у которого совпали паспортные данные и реквизиты аккаунта.
+     */
     fun findByRequisite(passport: String, requisite: String): Account? {
         return findByPassport(passport)?.let {
             user -> users[user]?.find { it -> it.requisite == requisite }
         }
     }
 
+    /**
+     * Метод осуществляет перевод с аккаунта с данными: паспорт - [srcPassport], реквизиты - [srcRequisite]
+     * на аккаунт с данными: паспорт - [destPassport], реквизиты - [destRequisite].
+     * @return true когда перевод был осуществлен, или false в следующих случаях:
+     * 1. Аккаунт, с которого осущетвляется перевод не существует;
+     * 2. Баланс аккаунта, с которого осуществляется перевод, меньше суммы [amount];
+     * 3. Аккаунт на который осуществляется перевод не существует.
+     */
     fun transferMoney(srcPassport: String, srcRequisite: String,
                       destPassport: String, destRequisite: String, amount: Double): Boolean {
         val srcAccount = findByRequisite(srcPassport, srcRequisite)
